0db609a21c13c8b0e605dc8a86d65708c68c8470
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-27 14:05:05 +0100

    created a basic version of the game

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 # We are going to write a program that generates a random number and asks the user to guess it.
 
 # If the player’s guess is higher than the actual number, the program displays “Lower number please”. Similarly, if the user’s guess is too low, the program prints “higher number please” When the user guesses the correct number, the program displays the number of guesses the player used to arrive at the number.
+
+from random import randint
+
+rand = randint(0,10)
+guesses = 0
+
+user_guess = -1
+
+while(user_guess != rand):
+    guesses += 1
+    user_guess = int(input("Guess a number between 0-10: "))
+
+    if user_guess > rand:
+        print("Guess a lower number.")
+    elif user_guess < rand:
+        print("Guess a higher number.")
+
+print(f"Congratulations! You guessed the correct number {rand}")
+print(f"Your guesses: {guesses}")
